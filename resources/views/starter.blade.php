c8a540f8951a55c4cbbccc20901dd57e86829785
minhnguyen24041999 <minhndqph05606@fpt.edu.vn>
2019-10-05 18:54:15 +0700

    commit

---
+++ resources/views/starter.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts')
+
+@section('title', 'Starter')
+
+@section('contents')
+ <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Page Header
+        <small>Optional description</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
+    @include('sidebar')
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+
+    </section>
+    <!-- /.content -->
+    <a href="{{ route('users.create')}}" class="btn btn-success">Create</a>
+    @if(empty($users))
+     <p>No Data</p>
+    @else
+     <table class="table">
+      <thead>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Birthday</th>
+      </thead>
+
+      <tbody>
+        @foreach($users as $user)
+          <tr>
+            <td>{{$user['name']}}</td>
+            <td>{{$user['email']}}</td>
+            <td>{{$user['birthday']}}</td>
+            <td><a href="#" class="btn btn-primary">Update</a>
+            <td><a href="#" class="btn btn-danger">Delete</a>
+          </tr>
+        @endforeach
+        @endif
+      </tbody>
+    </table>
+  </div>
+@endsection
